163995e893f0cf0302451fd8e3801b4e0b17f138
eftinur <eftinur90@gmail.com>
2023-08-10 06:48:37 +0600

    finished image upload feature

--- src/comps/UploadForm.jsx
+++ src/comps/UploadForm.jsx
@@ -1,22 +1,47 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import { AUTH_CONTEXT } from "../contextAPI/AuthProvider";
 
 const UploadForm = () => {
-  const Image_Hosting_Token = import.meta.env.VITE_Image_Hosting_Token;
-  console.log(Image_Hosting_Token);
-
   const [selectedFile, setSelectedFile] = useState(null);
+  // user_data
+  const { user } = useContext(AUTH_CONTEXT);
+
+  const Image_Hosting_Token = import.meta.env.VITE_Image_Hosting_Token;
+
+  const url = `https://api.imgbb.com/1/upload?key=${Image_Hosting_Token}`;
+
+  // Getting uploaded files from Input Field
   const handleUploads = (e) => {
     if (e.target.files && e.target.files[0]) {
       setSelectedFile(e.target.files[0]);
-      console.log(e.target.files);
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     if (selectedFile) {
       // upload files to the storage
-      // console.log(selectedFile);
+      // Appending images in FormData
+      const formData = new FormData();
+      formData.append("image", selectedFile);
+
+      // Storing Images in Hosting
+      axios.post(url, formData).then((image) => {
+        console.log(image);
+        // Creating New Post Object
+        const newPost = {
+          display_url: image.data.data.display_url,
+          user_email: user.email,
+        };
+
+        // Sending Upload Data to the Database
+        axios.post("http://localhost:5000/posts", newPost).then((data) => {
+          console.log(data.data);
+          // Force a Form reset
+        });
+      });
     }
   };
 
